2696913b010eed2d98c6805862f89385bae5baeb
Alberto Hernandez Cerezo <pascu216@gmail.com>
2024-12-21 07:34:50 -0500

    feat: Mouse Raycaster
    To hit the ball, we want to use a swipe mechanic where the user drags the finger on the screen to aim and shoot.
    
    For this to work as expected, we need to transform the viewport coordinates of the player gestures into coordinates of the 3D world. For that, I implemented this script.
    
    The script uses the given camera, object reference, and plane to translate viewport 2D coordinates to space 3D coordinates.
    
    We use `project_ray_origin` to project the viewport 2D coordinates in a 3D space corresponding to the 2D dot in the 3D plane represented by the orthogonal camera. We combine this point with a direction vector parallel to the camera direction, and get the intersection of this vector with an horizontal plane set at the ball height.

---
+++ scenes/player/Player.gd
@@ -0,0 +1,74 @@
+extends Node3D
+
+# Ball shooting parameters
+var cursor_3d_position_on_aim_start: Vector3
+var force_and_direction_vector_for_shot: Vector3
+
+# Dependent nodes
+@onready var ball: RigidBody3D = $Ball
+@onready var camera: Camera3D = $CameraWrapper/Camera
+@onready var mouse_raycaster: MouseRaycaster = $MouseRaycaster
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_set_up_mouse_raycaster()
+
+## Configure mouse raycaster. Required for mini-golf ball aiming mechanic
+func _set_up_mouse_raycaster() -> void:
+	mouse_raycaster.setup(camera, ball, Plane(Vector3.UP))
+
+func _physics_process(delta: float) -> void:
+	if aiming: _refresh_force_and_direction_vector_for_shot()
+
+## Input logic
+## -----------
+#  (!) -> To be moved to a script
+var aiming: bool = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_mouse_button"):
+		_start_aiming()
+	elif event.is_action_released("left_mouse_button"):
+		_shot()
+
+## Resets aiming parameters and sets aim flag
+## to true for tracking mouse coordinates
+func _start_aiming() -> void:
+	aiming = true
+	force_and_direction_vector_for_shot = Vector3()
+
+	var new_cursor_3d_position_on_aim_start: Variant = mouse_raycaster.mouse_viewport_coordinates_in_3d_space()
+	
+	if new_cursor_3d_position_on_aim_start != null:
+		cursor_3d_position_on_aim_start = new_cursor_3d_position_on_aim_start
+
+	print_debug("player started aiming at position %s" % cursor_3d_position_on_aim_start)
+
+## Resets aiming parameter and sets aim flag
+## to false to stop tracking mouse coordinates
+## and cancel ongoing shot (if not triggered already)
+func _stop_aiming() -> void:
+	print_debug("player stopped aiming")
+
+	aiming = false
+	cursor_3d_position_on_aim_start = Vector3()
+	force_and_direction_vector_for_shot = Vector3()
+
+## Hits the ball with the direction and force the
+## user is aiming for. Resets aiming coordinates
+## for future shots.
+func _shot() -> void:
+	print_debug("player shots ball")
+
+	if aiming:
+		_stop_aiming()
+
+## Updates shot direction and force vector based
+## on initial and current rayscasted cursor positions
+func _refresh_force_and_direction_vector_for_shot() -> void:
+	if aiming:
+		var current_cursor_3d_position: Variant = mouse_raycaster.mouse_viewport_coordinates_in_3d_space()
+
+		if current_cursor_3d_position == null || cursor_3d_position_on_aim_start == null: return
+
+		force_and_direction_vector_for_shot = current_cursor_3d_position - cursor_3d_position_on_aim_start
---
+++ scripts/MouseRaycaster.gd
@@ -0,0 +1,55 @@
+extends Node3D
+
+## Implement the logic to transform mouse coordinates
+## in the viewport to its equivalent in the 3D space.
+##
+## Given a 3D infinite plane and a 3D camera, the script
+## uses the cursor viewport coordinates to calculate its
+## equivalent position in the 3D space as the intersection
+## of a ray projected from the camera to the plane.
+class_name MouseRaycaster
+
+var raycast_camera: Camera3D
+var raycast_plane: Plane
+var raycast_body_reference: Node3D
+
+## Configure mouse raycast to start calculating mouse positions
+func setup(camera: Camera3D, body_reference: Node3D, plane: Plane = Plane(Vector3.UP)) -> void:
+	raycast_body_reference = body_reference
+	raycast_camera = camera
+	raycast_plane = plane
+
+## Is the raycast ready to transform mouse coordinates?
+func ready_to_raycast() -> bool:
+	return raycast_camera == null || raycast_plane == null
+
+## Returns the position of the cursor in the 3D space, result
+## of projecting it from the given rayscast camera to the given
+## 3D plane.
+## 
+## (!) Only compatible for orthogonal cameras. In perspective camera
+##     our view is composed by a set of vectors, originated from the
+##     same point in space (hence its "perspective" name) and with
+##     different directions. For an orthogonal cameras, all vectors
+##     composing the actual view share the same direction, but with
+##     different origins. Thus, this method relies on the camera
+##     basis direction and the project_ray_origin instead of the
+##     project_ray_normal method.
+func mouse_viewport_coordinates_in_3d_space() -> Variant:
+	if !ready_to_raycast():
+		push_error("MouseRaycaster not configured. Ensure a camera, ball and plane are assigned to node instance")
+
+	raycast_plane.d = raycast_body_reference.global_position.y
+
+	# Viewport 2D cursor coordinates
+	var mouse_position_2d := get_viewport().get_mouse_position()
+	# 3D camera direction (always the same for orthogonal cameras)
+	var camera_direction := -raycast_camera.global_transform.basis.z.normalized()
+	# 3D point corresponding to orthogonal projection of viewport mouse position
+	var raycast_mouse_position := raycast_camera.project_ray_origin(mouse_position_2d)
+
+	# Intersection of ray with camera direction and origin set to the
+	# projection of the cursor viewport coordinates in the 3D space
+	var raycast_intersection_point = raycast_plane.intersects_ray(raycast_mouse_position, camera_direction)
+
+	return raycast_intersection_point
