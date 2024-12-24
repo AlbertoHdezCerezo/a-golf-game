7170d118ac0291353b4df7438176bac009597067
Alberto Hernandez Cerezo <pascu216@gmail.com>
2024-12-24 11:23:57 -0500

    feat: Adjust physics
    I want the ball to emulate realistic physics, slowing down due to friction.
    
    It seems, the physics engine does not handle well rotation due to the small contact surface of the ball with the floor. Thus, I had to tweak the `angular` `damp` parameter of the ball rigid body to simulate this friction effect.
    
    There is still a lot to do in order to achieve a realistic simulation of the ball moving, but for now it should be enough. By playing with the angular damp and the force applied to the ball it is possible to get a more realistic effect.

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -15,7 +15,8 @@ var force_and_direction_vector_for_shot: Vector3
 @onready var mouse_raycaster := MouseRaycaster.new(camera, ball, Plane(Vector3.UP))
 
 func _physics_process(_delta: float) -> void:
-	if aiming: _refresh_force_and_direction_vector_for_shot()
+	if aiming && _can_shoot():
+		_refresh_force_and_direction_vector_for_shot()
 
 ## Input logic
 ## -----------
@@ -28,44 +29,50 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_released("left_mouse_button_down"):
 		_shot()
 
+func _can_shoot() -> bool:
+	# TODO: figure out why there is some velocity when
+	# 		ball is stale. Something wrong with physics?
+	return true
+
 ## Resets aiming parameters and sets aim flag
 ## to true for tracking mouse coordinates
 func _start_aiming() -> void:
-	aiming = true
-	force_and_direction_vector_for_shot = Vector3()
+	if _can_shoot():
+		aiming = true
+		force_and_direction_vector_for_shot = Vector3()
 
-	var new_cursor_3d_position_on_aim_start: Variant = mouse_raycaster.mouse_viewport_coordinates_in_3d_space()
-	
-	if new_cursor_3d_position_on_aim_start != null:
-		cursor_3d_position_on_aim_start = new_cursor_3d_position_on_aim_start
+		var new_cursor_3d_position_on_aim_start: Variant = mouse_raycaster.mouse_viewport_coordinates_in_3d_space()
+		
+		if new_cursor_3d_position_on_aim_start != null:
+			cursor_3d_position_on_aim_start = new_cursor_3d_position_on_aim_start
 
 ## Resets aiming parameter and sets aim flag
 ## to false to stop tracking mouse coordinates
 ## and cancel ongoing shot (if not triggered already)
 func _stop_aiming() -> void:
-	aiming = false
-	cursor_3d_position_on_aim_start = Vector3()
-	force_and_direction_vector_for_shot = Vector3()
+	if aiming:
+		aiming = false
+		cursor_3d_position_on_aim_start = Vector3()
+		force_and_direction_vector_for_shot = Vector3()
 
 ## Hits the ball with the direction and force the
 ## user is aiming for. Resets aiming coordinates
 ## for future shots.
 func _shot() -> void:
-	print_debug("player shots ball")
-
-	if aiming:
+	if aiming && _can_shoot():
 		hud.hide_hud()
-		ball.apply_impulse(-force_and_direction_vector_for_shot)
+		ball.apply_impulse(-force_and_direction_vector_for_shot * 5)
 		_stop_aiming()
 
 ## Updates shot direction and force vector based
 ## on initial and current rayscasted cursor positions
 func _refresh_force_and_direction_vector_for_shot() -> void:
-	if aiming:
-		var current_cursor_3d_position: Variant = mouse_raycaster.mouse_viewport_coordinates_in_3d_space()
+	if not aiming: return
 
-		if current_cursor_3d_position == null || cursor_3d_position_on_aim_start == null: return
+	var current_cursor_3d_position: Variant = mouse_raycaster.mouse_viewport_coordinates_in_3d_space()
 
-		force_and_direction_vector_for_shot = (current_cursor_3d_position - cursor_3d_position_on_aim_start).limit_length(MAX_MODULE_FOR_SHOT_VECTOR)
+	if current_cursor_3d_position == null || cursor_3d_position_on_aim_start == null: return
 
-		hud.draw_hud(force_and_direction_vector_for_shot)
+	force_and_direction_vector_for_shot = (current_cursor_3d_position - cursor_3d_position_on_aim_start).limit_length(MAX_MODULE_FOR_SHOT_VECTOR)
+
+	hud.draw_hud(force_and_direction_vector_for_shot)
