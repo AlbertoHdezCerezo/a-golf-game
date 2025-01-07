46810cbc46d16f1843fe19cf09b96033eb3b6ecb
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-01-07 20:28:52 +0100

    chore: Level scene logic
    Defines the logic of a level. A level is composed of a map, a starting position for the player ball and a finish area (where the ball should stay to consider the level completed).
    
    The node uses a timer to count the amount of time the ball stays in the hole, confirming it stays there and does not leave before sending the win state.
    
    The game scene will use this information to determine whether the player object is in the hole and the game is over.

---
+++ scenes/Level/Level.gd
@@ -0,0 +1,45 @@
+extends Node3D
+
+class_name Level
+
+signal object_entered_finish_area_signal(rigid_body_in_finish_area)
+signal object_left_finish_area_signal(rigid_body_which_left_finish_area)
+signal object_stayed_in_finish_area_until_timer_ran_out_signal(rigid_body_in_finish_area)
+
+@export var timeout_for_stay := 3
+@onready var finish_timer := $FinishTimer # Used to determine when the ball reaches the finish hole
+@onready var start_point := $StartPoint # Marks the begining of the level, where the ball will be loaded
+@onready var finish_area := $FinishArea # Marks the area representing the finish hole
+
+var body_in_finish_area: Node3D
+
+func _ready() -> void:
+	_listen_to_finish_area_events()
+	_listen_to_timer_events()
+
+func _listen_to_finish_area_events() -> void:
+	finish_area.body_entered.connect(_start_timer_and_notify_body_entered_finish_area)
+	finish_area.body_exited.connect(_stop_timer_and_notify_body_left_finish_area)
+
+func _listen_to_timer_events() -> void:
+	finish_timer.timeout.connect(_notify_body_stayed_until_timer_timeout)
+
+func _start_timer_and_notify_body_entered_finish_area(body: Node3D) -> void:
+	body_in_finish_area = body
+
+	emit_signal("object_entered_finish_area_signal", body_in_finish_area)
+
+	finish_timer.wait_time = timeout_for_stay
+	finish_timer.one_shot = true
+	finish_timer.start()
+
+func _stop_timer_and_notify_body_left_finish_area(body: Node3D) -> void:
+	if body == body_in_finish_area:
+		body_in_finish_area = null
+
+		emit_signal("object_left_finish_area_signal", body)
+
+		finish_timer.stop()
+
+func _notify_body_stayed_until_timer_timeout() -> void:
+	emit_signal("object_stayed_in_finish_area_until_timer_ran_out_signal", body_in_finish_area)
--- scenes/game/Game.gd
+++ scenes/game/Game.gd
@@ -2,12 +2,14 @@ extends Node3D
 
 var cursor_image: Resource = load("res://assets/images/cursors/pointer.svg")
 
-@onready var camera = $CameraWrapper/Camera
+@onready var camera := $CameraWrapper/Camera
+@onready var level := $Level
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_setup_game_cursor()
 	_setup_player()
+	_setup_level()
 
 # Updates game cursor with custom skin
 func _setup_game_cursor() -> void:
@@ -21,6 +23,13 @@ func _setup_player() -> void:
 	
 	# TODO: refactor this for a cleaner way to set the ball.
 	#		Avoid changing the node global position or it will break HUD display
-	player.get_node("Ball").position = player.get_node("Ball").position + Vector3(0,1.5,0)
+	var level_start_position = level.start_point.global_position
+	player.get_node("Ball").position = level_start_position + Vector3(0,1.5,0)
 
 	add_child(player)
+
+func _setup_level() -> void:
+	level.object_stayed_in_finish_area_until_timer_ran_out_signal.connect(_check_if_level_is_completed)
+
+func _check_if_level_is_completed(rigid_body_in_finish_area: Node3D) -> void:
+	print(rigid_body_in_finish_area)
