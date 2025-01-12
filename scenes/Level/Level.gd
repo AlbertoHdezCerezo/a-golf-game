02736b196a316047dddf5d573bf7ff5986c24e6b
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-01-12 18:52:25 +0100

    feat: update Level scene logic to handle LevelResource resources
    Now the `Level` scene can handle `LevelResource`s. When the `Level``level_resource` attribute is set, it triggers a similar logic of the `LevelEditor`, loading the map and updating the level start point.
    
    There is still some work to do in terms of adding a way to dumping the end area of the levels in the `LevelEditor`, and then load it in the `Level` scene, but for now we can already load different levels in a `Level` scene.

--- scenes/Level/Level.gd
+++ scenes/Level/Level.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node3D
 
 class_name Level
@@ -6,20 +7,34 @@ signal object_entered_finish_area_signal(rigid_body_in_finish_area)
 signal object_left_finish_area_signal(rigid_body_which_left_finish_area)
 signal object_stayed_in_finish_area_until_timer_ran_out_signal(rigid_body_in_finish_area)
 
-@export var level_resource : LevelResource
+@export var level_resource : LevelResource: 
+	set(new_level_resource):
+		level_resource = new_level_resource
+
+		if new_level_resource != null:
+			_load_level_from_level_resource()
+		else:
+			_reset_level()
+
 @export var timeout_for_stay := 3
 
+@onready var level_start_point := $StartPoint # Marks the begining of the level, where the ball will be loaded
 @onready var finish_timer := $FinishTimer # Used to determine when the ball reaches the finish hole
-@onready var start_point := $StartPoint # Marks the begining of the level, where the ball will be loaded
 @onready var finish_area := $FinishArea # Marks the area representing the finish hole
-@onready var grid_map := $Map
+@onready var level_map_grid := $Map
 
 var body_in_finish_area: Node3D
 
 func _ready() -> void:
-	_listen_to_finish_area_events()
-	_listen_to_timer_events()
-	_dump_grid_map_to_resource()
+	# Whenever the scene is loaded, either in the game or in the
+	# editor, clear all level information. Let the game or the
+	# developer set the level to play, and then proceed to set it up
+	level_resource = null
+	_reset_level()
+
+	if not Engine.is_editor_hint():
+		_listen_to_finish_area_events()
+		_listen_to_timer_events()
 
 func _listen_to_finish_area_events() -> void:
 	finish_area.body_entered.connect(_start_timer_and_notify_body_entered_finish_area)
@@ -40,19 +55,34 @@ func _start_timer_and_notify_body_entered_finish_area(body: Node3D) -> void:
 func _stop_timer_and_notify_body_left_finish_area(body: Node3D) -> void:
 	if body == body_in_finish_area:
 		body_in_finish_area = null
-
 		emit_signal("object_left_finish_area_signal", body)
-
 		finish_timer.stop()
 
 func _notify_body_stayed_until_timer_timeout() -> void:
 	emit_signal("object_stayed_in_finish_area_until_timer_ran_out_signal", body_in_finish_area)
 
-func _dump_grid_map_to_resource() -> void:
-	level_resource.map_grid_coordinates = grid_map.get_used_cells()
-	level_resource.map_grid_mesh_indexes = []
+# Recreates the level described by a LevelResource instance in the game level
+# TODO: very similar codewise to the LevelEditor logic, maybe worth exploring refactoring?
+func _load_level_from_level_resource() -> void:
+	_reset_level()
 
-	for used_cell_coordinate in level_resource.map_grid_coordinates:
-		level_resource.map_grid_mesh_indexes.append(grid_map.get_cell_item(used_cell_coordinate))
+	# Load mesh library in grid map
+	if level_resource.map_mesh_library != null:
+		level_map_grid.mesh_library = level_resource.map_mesh_library
 
-	ResourceSaver.save(level_resource, level_resource.resource_path)
+	# Update start point marker global position
+	if level_resource.start_point_position != null:
+		level_start_point.position = level_resource.start_point_position
+
+	# Draw grid map according to level tiles information
+	for index in level_resource.map_grid_coordinates.size():
+		var grid_tile_coordinate := level_resource.map_grid_coordinates[index]
+		var grid_tile_mesh_index := level_resource.map_grid_mesh_indexes[index]
+		var grid_tile_mesh_orientation := level_resource.map_grid_mesh_orientations[index]
+		level_map_grid.set_cell_item(grid_tile_coordinate, grid_tile_mesh_index, grid_tile_mesh_orientation)
+
+# Resets all variables in Level scene
+func _reset_level() -> void:
+	level_map_grid.mesh_library = null
+	level_start_point.position = Vector3(0,0,0)
+	level_map_grid.clear()
