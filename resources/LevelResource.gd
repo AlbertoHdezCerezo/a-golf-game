fecda4a233cb79b1be9801f97b5ab367067cfd53
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-01-12 18:00:27 +0100

    progress: Level Editor
    Refactors part of the Level Resource and Level Editor logic to reduce coupling.
    
    Now the editor is working. I am able to dump my progress in the editor when running the LevelEditor scene, and loading it when I load a level resource.
    
    There is still some issue to address. I am storing the meshes, but I am missing certain information, such as the rotation, which makes not possible to restore 100% accurately the map.

--- resources/LevelResource.gd
+++ resources/LevelResource.gd
@@ -2,4 +2,36 @@ extends Resource
 
 class_name LevelResource
 
-@export var start_point_global_position: Vector3
+@export var start_point_position: Vector3
+# Mesh library used to draw maps
+@export var map_mesh_library: MeshLibrary
+# Grid-map coordinates where tiles are placed
+@export var map_grid_coordinates: Array[Vector3i]
+# Tile index from mesh library.
+@export var map_grid_mesh_indexes: Array[int]
+
+# TODO: we might need to implement a validation system in future
+func can_save() -> bool:
+	var can_save_level_resource := true
+
+	if (map_mesh_library == null):
+		can_save_level_resource = false
+		push_warning("Level in editor cannot be saved: missing grid map mesh library")
+
+	if (map_grid_coordinates == null) || (map_grid_coordinates.size() == 0):
+		can_save_level_resource = false
+		push_warning("Level in editor cannot be saved: level grid map is empty")
+
+	if (start_point_position == null):
+		can_save_level_resource = false
+		push_warning("Level in editor cannot be saved: missing level start point marker")
+	
+	return can_save_level_resource
+
+# Dumps information from LevelEditor instance in the
+# LevelResource instance loaded within the scene.
+func dump_changes_to_resource_file() -> void:
+	if not can_save():
+		push_error("Failed to dump work from Level Editor in level resource")
+	else:
+		ResourceSaver.save(self, self.resource_path)
--- scenes/Level/Level.gd
+++ scenes/Level/Level.gd
@@ -7,17 +7,19 @@ signal object_left_finish_area_signal(rigid_body_which_left_finish_area)
 signal object_stayed_in_finish_area_until_timer_ran_out_signal(rigid_body_in_finish_area)
 
 @export var level_resource : LevelResource
-
 @export var timeout_for_stay := 3
+
 @onready var finish_timer := $FinishTimer # Used to determine when the ball reaches the finish hole
 @onready var start_point := $StartPoint # Marks the begining of the level, where the ball will be loaded
 @onready var finish_area := $FinishArea # Marks the area representing the finish hole
+@onready var grid_map := $Map
 
 var body_in_finish_area: Node3D
 
 func _ready() -> void:
 	_listen_to_finish_area_events()
 	_listen_to_timer_events()
+	_dump_grid_map_to_resource()
 
 func _listen_to_finish_area_events() -> void:
 	finish_area.body_entered.connect(_start_timer_and_notify_body_entered_finish_area)
@@ -45,3 +47,12 @@ func _stop_timer_and_notify_body_left_finish_area(body: Node3D) -> void:
 
 func _notify_body_stayed_until_timer_timeout() -> void:
 	emit_signal("object_stayed_in_finish_area_until_timer_ran_out_signal", body_in_finish_area)
+
+func _dump_grid_map_to_resource() -> void:
+	level_resource.map_grid_coordinates = grid_map.get_used_cells()
+	level_resource.map_grid_mesh_indexes = []
+
+	for used_cell_coordinate in level_resource.map_grid_coordinates:
+		level_resource.map_grid_mesh_indexes.append(grid_map.get_cell_item(used_cell_coordinate))
+
+	ResourceSaver.save(level_resource, level_resource.resource_path)
