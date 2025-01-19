5285c072c27259b39257f20154444d06aa67ebfc
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-01-19 20:04:47 +0100

    chore: `LevelSelector` Levels positioning in 3D space
    Extended original `LevelSelector` logic for instantiating `Level` scenes for previsualization.
    
    Now each level is rendered in a different position, so they do not overlap with each other. Level instances are wrapped inside a `Carrousel` node, whose purpose is to be manipulated in order to display the different levels in front of the camera.

--- scenes/LevelSelector/LevelSelector.gd
+++ scenes/LevelSelector/LevelSelector.gd
@@ -16,8 +16,12 @@ const LEVEL_SCENE = preload("res://scenes/Level/Level.tscn")
 		
 		_instantiate_game_levels()
 
-@onready var game_camera_wrapper := $LevelSelectorCameraWrapper
 @onready var game_camera := $LevelSelectorCameraWrapper/LevelSelectorCamera
+@onready var game_camera_wrapper := $LevelSelectorCameraWrapper
+
+# Node holding all level instances. We apply transformations to this
+# node in order to put each different level in front of the camera.
+var levels_carrousel : Node3D
 
 func _ready() -> void:
 	_setup_camera()
@@ -28,6 +32,10 @@ func _setup_camera():
 func _instantiate_game_levels() -> void:
 	var index := 1
 	var game_level_instance : Level
+	var next_game_level_position := Vector3.ZERO
+	
+	levels_carrousel = Node3D.new()
+	self.add_child(levels_carrousel)
 
 	for game_level_resource in game_level_resources:
 		# TODO: add meta information to level resources (name, number, etc)
@@ -35,7 +43,15 @@ func _instantiate_game_levels() -> void:
 		game_level_instance = LEVEL_SCENE.instantiate()
 		game_level_instance.name = "Level" + str(index)
 		game_level_instance.level_resource = game_level_resource
+		game_level_instance.position = next_game_level_position
 
-		self.add_child(game_level_instance)
+		levels_carrousel.add_child(game_level_instance)
 
 		index += 1
+		next_game_level_position = _level_position_in_selector(game_level_instance)
+
+# Determines where each one of the levels being rendered by the selector
+# need to be rendered in the global 3D space. Otherwise all levels would
+# appear overlapped.
+func _level_position_in_selector(last_game_level_instantiated) -> Vector3:
+	return last_game_level_instantiated.position + Vector3(7, 0, -7)
