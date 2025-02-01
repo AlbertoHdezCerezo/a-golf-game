c259a59d0c1aa65a3d3b94fa101a3b4d015f15ea
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-02-01 16:44:17 +0100

    chore: Refactor `DragAndDrop` controller
    Renames `DragAndDrop` controller as `DragAndDropInputHandler`. Now the handler directly processes the input event, which removes the responsibility of the `Player` node to pass `input` events down to the controller instance. This keeps the overall logic way more simplified, as initially intended.

--- scripts/input_handlers/drag_and_drop_input_handler.gd
+++ scripts/input_handlers/drag_and_drop_input_handler.gd
@@ -1,20 +1,20 @@
-extends Object
+extends InputHandler
 
-## Implements the drag and drop logic controller to hit
-## the ball on a specific direction and with a specific
-## force based on the player drag and drop gestures:
-##
-## 1. User presses the screen to start aiming.
-## 2. User moves finger to aim and set the shot force.
-## 3. User releases screen to shoot.
-##
-## The controller transforms input coordinates from the
-## 3D space (translated from the screen by the raycaster
-## in a normalized vector representing the direction and
-## strength of the shot). The game logic can then use
-## this information translate this vector into the res-
-## pective ball impulse.
-class_name DragAndDropController
+# Implements the drag and drop logic controller to hit
+# the ball on a specific direction and with a specific
+# force based on the player drag and drop gestures:
+#
+# 1. User presses the screen to start aiming.
+# 2. User moves finger to aim and set the shot force.
+# 3. User releases screen to shoot.
+#
+# The controller transforms input coordinates from the
+# 3D space (translated from the screen by the raycaster
+# in a normalized vector representing the direction and
+# strength of the shot). The game logic can then use
+# this information translate this vector into the res-
+# pective ball impulse.
+class_name DragAndDropInputHandler
 
 signal start_drag_signal
 signal drag_signal(force_and_direction_normalized_vector)
@@ -29,8 +29,6 @@ var dragging: bool
 
 ## Used to translate mouse coordinates from viewport to 3D space
 var mouse_raycaster: MouseRaycaster
-## Min and max values used to normalize vector
-## resulting from drag and drop coordinates.
 var min_direction_and_force_vector_length
 var max_direction_and_force_vector_length
 
@@ -40,25 +38,16 @@ func _init(
 	new_max_direction_and_force_vector_length: float = 1
 ) -> void:
 	mouse_raycaster = new_mouse_raycaster
-	
-	if new_min_direction_and_force_vector_length >= new_max_direction_and_force_vector_length:
-		push_error("Drag & Drop controller expects valid min-max range to normalize force and direction vector. Given range min is greater or equal than max value.")
-	
 	min_direction_and_force_vector_length = new_min_direction_and_force_vector_length
 	max_direction_and_force_vector_length = new_max_direction_and_force_vector_length
 
-## Updates drag and drop coordinates based on mouse
-## Emits signals based on events kind to notify parent
-## nodes about completion of drag & drop gesture
-func handle_event(event: InputEvent) -> Variant:
-	if event.is_action_pressed("left_mouse_button_down"):
+func _handle_event(event: InputEvent) -> void:
+	if event.is_action_pressed("drag"):
 		_start_drag()
 	if event is InputEventMouseMotion && dragging:
 		_drag()
-	elif event.is_action_released("left_mouse_button_down") && dragging:
+	elif event.is_action_released("drag") && dragging:
 		_drop()
-	
-	return normalized_direction_and_force_vector
 
 ## Starts tracking drag & drop gesture by storing the initial
 ## gestures coordinates in the respective attributes
