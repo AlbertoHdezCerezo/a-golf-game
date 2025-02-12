46c852c5ffa22753478a2b1dbf1a6b29c4e025ba
Alberto Hernandez Cerezo <pascu216@gmail.com>
2025-02-12 19:53:44 +0100

    feat: LevelManager
    Adds a new `LevelManager` class. This class is responsible of loading all game levels in memory, from their respective resources.
    
    Now the `LevelNavigator` uses the `LevelManager` to access to the levels available in game, and navigate between those.
    
    The PR also refactors parts of the existing logic to simplify the current logic

--- scenes/LevelNavigator/LevelNavigator.gd
+++ scenes/LevelNavigator/LevelNavigator.gd
@@ -11,50 +11,47 @@ const LEVEL_SCENE = preload("res://scenes/Level/Level.tscn")
 
 var slider_tween : Tween
 var level_slider : Node3D
-var level_resources: Array[LevelResource]
-var current_level_resource_index : int
+var level_manager : LevelManager
 
-func _init(game_level_resources: Array[LevelResource]) -> void:
-	level_resources = game_level_resources
-	current_level_resource_index = 0
+func _init() -> void:
+	level_manager = LevelManager.new()
+	level_manager.load_all_levels_resources()
+
 	_initialize_level_slider()
-
-func current_level_resource() -> LevelResource:
-	return level_resources[current_level_resource_index]
-
-func current_level() -> Level:
-	return level_slider.get_child(current_level_resource_index)
 
 func navigate_to_adjactent_level(direction_for_adjacent_level: Vector2) -> void:
 	if direction_for_adjacent_level.x < 0:
-		current_level_resource_index = clamp(current_level_resource_index - 1, 0, level_resources.size() - 1)
+		level_manager.select_previous_level_resource()
 	if direction_for_adjacent_level.x > 0:
-		current_level_resource_index = clamp(current_level_resource_index + 1, 0, level_resources.size() - 1)
+		level_manager.select_next_level_resource()
 
 	# Rotates the slider in the oposite direction in which levels
 	# were rendered to face the adjacent level requested by the input
-	var new_level_slider_position := -1 * _position_for_level(current_level_resource())
+	var new_level_slider_position := -1 * _position_for_level_index(level_manager.current_level_resource_index)
 
 	if level_slider.position != new_level_slider_position:
 		slider_tween = level_slider.create_tween()
 		slider_tween.tween_property(level_slider, "position", new_level_slider_position, 0.5).set_trans(Tween.TRANS_SINE)
+
+func current_level() -> Level:
+	return level_slider.get_child(level_manager.current_level_resource_index)
 
 func _initialize_level_slider() -> void:
 	level_slider = Node3D.new()
 	level_slider.name = "LevelSlider"
 	self.add_child(level_slider)
 
-	var index := 1
+	var index := 0
 	var level_instance : Level
 
-	for level_resource in level_resources:
+	for level_resource in level_manager.level_resources:
 		level_instance = LEVEL_SCENE.instantiate()
 		level_instance.name = "Level" + str(index)
 		level_instance.level_resource = level_resource
-		level_instance.position = _position_for_level(level_resource)
+		level_instance.position = _position_for_level_index(index)
 		level_slider.add_child(level_instance)
 
 		index += 1
 
-func _position_for_level(level_resource: LevelResource) -> Vector3:
-	return level_resources.find(level_resource) * DISTANCE_BETWEEN_LEVELS * DIRECTION_FROM_ADJACENT_LEVELS_IN_SAME_WORLD
+func _position_for_level_index(level_index: int) -> Vector3:
+	return level_index * DISTANCE_BETWEEN_LEVELS * DIRECTION_FROM_ADJACENT_LEVELS_IN_SAME_WORLD
---
+++ scripts/utils/level_manager.gd
@@ -0,0 +1,42 @@
+extends Object
+
+# Handles LevelResource resources, providing an
+# interface to navigating between levels according
+# to their indexes
+class_name LevelManager
+
+const LEVEL_RESOURCES_PATH = "res://assets/resources/levels"
+
+var current_level_resource_index : int
+var level_resources : Array[LevelResource]
+
+func load_all_levels_resources() -> void:
+	level_resources = []
+
+	var dir := DirAccess.open(LEVEL_RESOURCES_PATH)
+	
+	for filepath in dir.get_files():
+		level_resources.append(ResourceLoader.load(LEVEL_RESOURCES_PATH + "/" + filepath))
+
+	level_resources.sort_custom(
+		func(lra: LevelResource, lrb: LevelResource) -> bool:
+			if lra.world_index == lrb.world_index:
+				return lra.index <= lrb.index
+			else:
+				return lra.world_index <= lrb.world_index
+	)
+
+	current_level_resource_index = 0
+
+func select_previous_level_resource() -> LevelResource:
+	if current_level_resource_index > 0: current_level_resource_index -= 1
+	
+	return current_level_resource()
+
+func select_next_level_resource() -> LevelResource:
+	if current_level_resource_index < level_resources.size() - 1: current_level_resource_index += 1
+
+	return current_level_resource()
+
+func current_level_resource() -> LevelResource:
+	return level_resources[current_level_resource_index]
